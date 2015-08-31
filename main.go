7d43e9e3559a50d6d30bafac1ceb0bdd2dc26cd5
sanathp <pasumarthy.sanath@gmail.com>
2015-09-01 02:26:55 +0530

    Create command line interface

--- main.go
+++ main.go
@@ -3,18 +3,28 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	_ "github.com/codegangsta/cli"
-	"github.com/gin-gonic/gin"
+	"github.com/codegangsta/cli"
 	"github.com/sanathp/StatusOk/database"
 	"github.com/sanathp/StatusOk/notify"
 	"github.com/sanathp/StatusOk/requests"
+	"io"
+	"net/http"
 	"os"
+	"strconv"
 )
 
 type configParser struct {
+	NotifyWhen    NotifyWhen               `json:"notifyWhen"`
 	Requests      []requests.RequestConfig `json:"requests"`
 	Notifications notify.NotificationTypes `json:"notifications"`
 	Database      database.DatabaseTypes   `json:"database"`
+	Concurrency   int                      `json:"concurrency"`
+	Port          int                      `json:"port"`
+}
+
+type NotifyWhen struct {
+	MeanResponseCount int `json:"meanResponseCount"`
+	ErrorCount        int `json:"errorCount"`
 }
 
 func main() {
@@ -23,13 +33,48 @@ func main() {
 	//TODO: test all these , write test cases
 	//TODO: format and comment code
 	//TODO: run it for 1 day with all diffrenet type of requests
-	//TODO: add cli tool
 	//TODO: validations for Config.Json file
 	//TODO: build a website using github pages
 	//TODO: create Docker file with complete setup
+	//TODO: run a deamon using upstaart . learn how to do it https://github.com/zaf/agitator/tree/master/init
+	//TODO: gracefull shutdown when user stops the app
 
-	r := gin.Default()
-	configFile, err := os.Open("test_config.json")
+	app := cli.NewApp()
+
+	app.Name = "StatusOk"
+	app.Usage = "Monitor your api.Get notifications when its down"
+
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "config",
+			Value: "config.json",
+			Usage: "location of config file",
+		},
+	}
+	app.Action = func(c *cli.Context) {
+		fileName := ""
+		if len(c.Args()) > 0 {
+			fileName = c.Args()[0]
+		}
+		if len(c.String("config")) == 0 {
+			println("Config parameter is mandatory ")
+		} else {
+			if fileExists(c.String("config")) {
+				println(" File location is", c.String("config"), fileName)
+				startServer(c.String("config"))
+			} else {
+				println("Config file not present at the given location: ", c.String("config"), "\nPlease give correct file location using --config parameter")
+			}
+		}
+	}
+
+	app.Run(os.Args)
+
+}
+
+func startServer(fileName string) {
+
+	configFile, err := os.Open(fileName)
 
 	if err != nil {
 		fmt.Println("Error opening config file", err.Error())
@@ -37,29 +82,48 @@ func main() {
 
 	jsonParser := json.NewDecoder(configFile)
 
-	var jsonData configParser
-	if err = jsonParser.Decode(&jsonData); err != nil {
-		fmt.Println("Error parsing config file", err.Error())
+	var config configParser
+	if err = jsonParser.Decode(&config); err != nil {
+		fmt.Println("Error parsing config file .Please check format of the file \nParse Error:", err.Error())
+		os.Exit(3)
 	}
 
-	database.AddNew(jsonData.Database)
-	notify.AddNew(jsonData.Notifications)
-
+	notify.AddNew(config.Notifications)
 	notify.SendTestNotification()
 
-	//TODO: send all requests for one time and stop of there are any errors
-	requests.RequestsInit(jsonData.Requests)
-	requests.StartMonitoring()
-
 	//Initialze urls map for monitoring
-
 	urls := make(map[string]int64)
 
-	for _, value := range jsonData.Requests {
+	for _, value := range config.Requests {
 		urls[value.Url] = value.ResponseTime
 	}
 
-	database.Initialize(urls, 3)
+	database.AddNew(config.Database)
+	database.Initialize(urls, config.NotifyWhen.MeanResponseCount, config.NotifyWhen.ErrorCount)
 
-	r.Run(":3143")
+	requests.RequestsInit(config.Requests, config.Concurrency)
+	requests.StartMonitoring()
+
+	//Tells whether Status Handler is running or not
+	http.HandleFunc("/", statusHandler)
+
+	if config.Port == 0 {
+		//Default
+		http.ListenAndServe(":7321", nil)
+	} else {
+		http.ListenAndServe(":"+strconv.Itoa(config.Port), nil)
+	}
+}
+
+func statusHandler(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "StatusOk is running")
+}
+
+func fileExists(name string) bool {
+	if _, err := os.Stat(name); err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+	}
+	return true
 }
